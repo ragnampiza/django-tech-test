2ab34fae66e60f5299909c75e8da6c176dbbb2b2
ragnampiza <krishan.vadher@gmail.com>
2017-03-13 23:58:03 +0000

    Add files via upload

--- users/forms.py
+++ users/forms.py
@@ -1,14 +1,15 @@
-#to take the input of firstname
-# to take the input of surname 
-# to take the input of email MAKE SURE THEY INCLUDE A VALID EMAIL ADDRESS THAT CONTAINS AN @ SIGN
-# to take the input of phone number KRISHAN MAKE SURE THEY CAN PUT THE CORRECT AMOUNT OF DIGITS IN ONLY
 from django import forms
+from django.core.validators import ValidationError
+from .models import User
+from django.core.validators import RegexValidator
+from django.core.validators import MinLengthValidator
+from django.core.validators import MaxLengthValidator
 from django.core.validators import MinValueValidator
 from django.core.validators import MaxValueValidator
+from django.core.validators import RegexValidator
+from .models import User
 
 class UserForm(forms.Form):
-
-
 
     firstname = forms.CharField(max_length=100)
     surname = forms.CharField(max_length=100)
@@ -18,22 +19,48 @@ class UserForm(forms.Form):
     company_street_address = forms.CharField(max_length=100)
     city = forms.CharField(max_length=100)
     postcode = forms.CharField(max_length=10)
-    company_number = forms.CharField(max_length=9)
-
+    #company_number = forms.CharField(validators=[RegexValidator(regex='^.{8}$', message='Length has to be 8 characters', code='nomatch')])
+    company_number = forms.CharField(max_length=8)
     filter_choices = (
         ('retail', 'Retail'),
         ('professional services', 'Professional Services'),
         ('food & drink', 'Food & Drink'),
         ('entertainment', 'Entertainment'),
     )
-    
     business_sector = forms.ChoiceField(choices = filter_choices)
-
     days = forms.CharField(max_length=5)
     reason_for_loan = forms.CharField(max_length=2000,widget=forms.Textarea)
-    
     borrowing = forms.IntegerField(validators=[MinValueValidator(10000),MaxValueValidator(100000)])
- 
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+    def clean_borrowing(self):
+       borrowing = self.cleaned_data['borrowing']
+       if not 10000 < borrowing < 100000:
+           raise forms.ValidationError("Your error message here")
+       return borrowing
+    
+    #'borrowing = forms.CharField(max_length=9)
+    #borrowing = forms.IntegerField(validators=[MinValueValidator(10000),MaxValueValidator(100000)])
+
+##    def clean_borrowing(self):
+##        borrowing = self.cleaned_data['borrowing']
+##        if not 10000 < borrowing < 100000:
+##            raise forms.ValidationError("Please enter a borrowing value between " \
+##                                    "10000 and 100000")
+##
+##        return borrowing
+##
+##
+##    borrowing = cleaned_data['borrowing']
+##    if not 10000 < borrowing < 100000:
+##        raise forms.ValidationError("Please enter a borrowing value between 10000 and 100000")
+##    else:
+##        borrowing = cleaned_data['borrowing']
+## 
 
 
 
--- users/models.py
+++ users/models.py
@@ -1,7 +1,13 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
+from django.core.validators import MaxLengthValidator
 from django.core.validators import MinValueValidator
 from django.core.validators import MaxValueValidator
+from django.core.validators import RegexValidator
 
+alphanumeric = RegexValidator(r'^[0-9a-zA-Z]*$', 'Only alphanumeric characters are allowed.')
+alpha = RegexValidator(r'^[zA-Z]*$', 'Only alpha characters are allowed.')
+numeric = RegexValidator(r'^[0-9a]*$', 'Only numeric characters are allowed.')
 
 class User(models.Model):
     #to store user data
@@ -9,16 +15,15 @@ class User(models.Model):
     firstname = models.CharField(max_length=100)
     surname = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
+    #telephone_number =  models.CharField('telephone_number', max_length=15, validators=[MinLengthValidator(7)])
     telephone_number = models.CharField(max_length=15)
     company_name = models.CharField(max_length=100)
     company_street_address = models.CharField(max_length=100)
     city = models.CharField(max_length=100)
-    postcode = models.CharField(max_length=10)
-    company_number = models.CharField(max_length=9)
+    postcode = models.CharField('postcode', max_length=9, validators=[MinLengthValidator(7)])
+    company_number = models.CharField(max_length=8)
+    #company_number = models.CharField(validators=[RegexValidator(regex='^.{8}$', message='Length has to be 8 characters', code='nomatch')])
     
-    #business_sector = models.CharField(max_length=100)
-    #business_sector = models.ChoiceField(choices = filter_choices)
-
     filter_choices = (
         ('retail', 'Retail'),
         ('professional services', 'Professional Services'),
@@ -26,24 +31,21 @@ class User(models.Model):
         ('entertainment', 'Entertainment'),
     )
     business_sector = models.CharField(max_length=100, choices=filter_choices)
-
-    #borrowing = models.IntegerField(choices=[(i, i) for i in range(1, 1)], blank=True)
-
-    #borrowing = models.IntegerField((validators=[MaxValueValidator(100),MinValueValidator(1)])
-
     days = models.CharField(max_length=5)
     reason_for_loan = models.CharField(max_length=2000)
+    borrowing = models.IntegerField(validators=[MinValueValidator(10000),MaxValueValidator(100000)])
 
-    borrowing = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(100)])
-
-    if 10000 <= borrowing <= 100000:
-    
+    #business_sector = models.CharField(max_length=100)
+    #business_sector = models.ChoiceField(choices = filter_choices)
+    #borrowing = models.CharField(max_length=100)
+    #borrowing = models.IntegerField(choices=[(i, i) for i in range(1, 1)], blank=True)
+    #borrowing = models.IntegerField((validators=[MaxValueValidator(100),MinValueValidator(1)])
+    #borrowing = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(100)])
+    #if 10000 <= borrowing <= 100000:
         #borrowing = models.CharField(max_length=100)
-        
+              
+    def __str__(self):
+        return self.firstname
 
-            
-        def __str__(self):
-            return self.firstname
-
-    else:
-        print "figure must be between 1000 100000"
+    #else:
+        #print "figure must be between 10000 100000"
